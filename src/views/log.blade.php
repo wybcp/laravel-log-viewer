3ad616048aefb0f861bf8ea91f9c9d912be0490a
Riverside <wangyb65@gmail.com>
2023-08-07 21:47:39 +0800

    Update log.blade.php
    change address

--- src/views/log.blade.php
+++ src/views/log.blade.php
@@ -5,11 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="robots" content="noindex, nofollow">
   <title>Laravel log viewer</title>
-  <link rel="stylesheet"
+<!--   <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+        crossorigin="anonymous"> -->
+  <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.bootcdn.net/ajax/libs/datatables/1.10.16/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<!--   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"> -->
   <style>
     body {
       padding: 25px;
